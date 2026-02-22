b9808886b9213e6b77550303baebd5d5bfaffcf5
hashiniRanasinghe <chathurangihashini5@gmail.com>
2026-02-22 11:39:44 +0530

    srilankan-food-trainer-0.2.1 - Updated version number in setup.py to reflect new release

--- srilankan-food-trainer - Python library/setup.py
+++ srilankan-food-trainer - Python library/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="srilankan-food-trainer",
-    version="0.1.0",
+    version="0.2.1",
     author="Hashini Ranasinghe",
     author_email="your.email@example.com",
     description="Easy-to-use library for extending Sri Lankan food recognition models",
